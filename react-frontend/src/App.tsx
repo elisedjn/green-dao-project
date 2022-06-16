ec40fc304c8d64388ea94d5dc3837f44d8912c46
Riley Chapman <chapman.riley@gmail.com>
2022-06-15 22:35:15 -0700

    first additions to members page

--- react-frontend/src/App.tsx
+++ react-frontend/src/App.tsx
@@ -9,7 +9,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/member/:memberId' element={<MemberPage />} />
+        <Route path='/member/' element={<MemberPage />} />
       </Routes>
     </BrowserRouter>
   );
