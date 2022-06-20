1b39fd35e8161cf104867674ac5e31c18af9e7ff
Elise Djn <elisedjn@protonmail.com>
2022-06-20 15:01:28 +0200

    image and project data upload to ipfs

--- react-frontend/src/Components/Molecules/ProjectCard.tsx
+++ react-frontend/src/Components/Molecules/ProjectCard.tsx
@@ -11,13 +11,12 @@ type ProjectCardType = {
 };
 
 const ProjectCard: React.FC<ProjectCardType> = ({ project, allowVote }) => {
-  const { image, title, description, id } = project;
+  const { image, title, description, address } = project;
 
   return (
     <Card className='project-card'>
       <CardMedia component='img' height='300' image={image} alt={title} />
       <CardContent className={allowVote ? 'project-content big-card' : 'project-content'}>
-
         <h3>{title}</h3>
         <p>{description}</p>
       </CardContent>
@@ -25,7 +24,7 @@ const ProjectCard: React.FC<ProjectCardType> = ({ project, allowVote }) => {
         <div className='fade' />
 
         {allowVote ? (
-          <Button onClick={() => console.log(id)} variant='text' className='link'>
+          <Button onClick={() => console.log(address)} variant='text' className='link'>
             Vote for this project
           </Button>
         ) : (
